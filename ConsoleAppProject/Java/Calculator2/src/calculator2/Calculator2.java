ce102bd6b24c6591e5e3a933842de2ac46f8a9a4
Thomas Harrison <100161949@student.burnley.ac.uk>
2022-02-02 15:28:26 +0000

    added java folder

---
+++ ConsoleAppProject/Java/Calculator2/src/calculator2/Calculator2.java
@@ -0,0 +1,35 @@
+/*
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package calculator2;
+
+public class Calculator2 
+{
+    String name;
+    
+    
+    public int add(int x, int y) 
+    {
+        return x + y;
+    }
+    
+    
+    public int subtract(int x, int y) 
+    {
+        return x - y;
+    }
+    public int multiply(int x, int y) 
+    {
+        return x * y;
+    }
+    public int divide(int x, int y) 
+    {
+        return x / y;
+    }
+    public int modulus(int x, int y) 
+    {
+        return x + y;
+    }
+}   
