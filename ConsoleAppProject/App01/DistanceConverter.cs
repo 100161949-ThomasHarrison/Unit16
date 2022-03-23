59038dfdb0e1f086b186797e2d0e095d72588e9c
Thomas Harrison <100161949@student.burnley.ac.uk>
2022-03-23 13:44:56 +0000

    ch

--- ConsoleAppProject/App01/DistanceConverter.cs
+++ ConsoleAppProject/App01/DistanceConverter.cs
@@ -8,6 +8,29 @@
     /// </author>
     public class DistanceConverter
     {
+        private double miles;
 
+        private double feet;
+
+        public void Run()
+        {
+            InputMiles();
+            CalulateFeet();
+            OutputFeet();
+        }
+        private void InputMiles()
+        {
+            Console.Write("Please enter the number of miles > ");
+            string.value = Console.ReadLine();
+            miles = Convert.ToDouble(value);
+        }
+        private void CalulateFeet()
+        {
+
+        }
+        private void OutputFeet()
+        {
+
+        }
     }
 }
